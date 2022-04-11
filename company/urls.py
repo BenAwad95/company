060eb681b4297d801810807d40094482e24f9a25
abdullah_awad <abdullah.alashafi@gmail.com>
2022-04-11 13:30:42 +0300

    nice app

--- company/urls.py
+++ company/urls.py
@@ -24,5 +24,5 @@ urlpatterns = [
 ]
 
 # for serve static files in development 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ department/models.py
@@ -0,0 +1,24 @@
+import imp
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+# Create your models here.
+
+class Department(models.Model):
+    name = models.CharField(_("Department name"), max_length=100)
+    manager = models.OneToOneField("Employee", verbose_name=_("Department's Manger"), on_delete=models.CASCADE, related_name='manger', null=True, blank=True)
+    
+    def __str__(self):
+        return self.name
+    
+
+class Employee(models.Model):
+    id = models.IntegerField(_("Employee's id"), unique=True, primary_key=True)
+    name = models.CharField(_("Employee name"), max_length=50)
+    email = models.EmailField(_("employee's email"), max_length=254, blank=True, null=True)
+    department = models.ForeignKey("Department", verbose_name=_("Employee department"), on_delete=models.CASCADE)
+    join_date = models.DateField(_("Employee's join date"), auto_now=False, auto_now_add=False)
+
+    def __str__(self):
+        return f'Employee No.{self.id} - {self.name}'
+    
